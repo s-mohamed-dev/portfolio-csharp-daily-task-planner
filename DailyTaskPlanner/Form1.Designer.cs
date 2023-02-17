23500a2966d344ddbf1e622798018aa9e7ed0fe8
s-mohamed-dev <s.mohamed.dev@gmail.com>
2023-02-17 12:50:13 +0100

    Initial push

---
+++ DailyTaskPlanner/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace DailyTaskPlanner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl_Main = new System.Windows.Forms.TabControl();
+            this.tabPage_TodoToday = new System.Windows.Forms.TabPage();
+            this.groupBox_Action = new System.Windows.Forms.GroupBox();
+            this.button_ActionModify = new System.Windows.Forms.Button();
+            this.button_ActionDelete = new System.Windows.Forms.Button();
+            this.button_ActionAdd = new System.Windows.Forms.Button();
+            this.button_ActionSave = new System.Windows.Forms.Button();
+            this.groupBox_TodoToday = new System.Windows.Forms.GroupBox();
+            this.checkedListBox_TodoToday = new System.Windows.Forms.CheckedListBox();
+            this.tabPage_TodoOld = new System.Windows.Forms.TabPage();
+            this.tabControl_Main.SuspendLayout();
+            this.tabPage_TodoToday.SuspendLayout();
+            this.groupBox_Action.SuspendLayout();
+            this.groupBox_TodoToday.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl_Main
+            // 
+            this.tabControl_Main.Controls.Add(this.tabPage_TodoToday);
+            this.tabControl_Main.Controls.Add(this.tabPage_TodoOld);
+            this.tabControl_Main.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl_Main.Location = new System.Drawing.Point(0, 0);
+            this.tabControl_Main.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tabControl_Main.Name = "tabControl_Main";
+            this.tabControl_Main.SelectedIndex = 0;
+            this.tabControl_Main.Size = new System.Drawing.Size(1258, 664);
+            this.tabControl_Main.TabIndex = 6;
+            // 
+            // tabPage_TodoToday
+            // 
+            this.tabPage_TodoToday.Controls.Add(this.groupBox_Action);
+            this.tabPage_TodoToday.Controls.Add(this.groupBox_TodoToday);
+            this.tabPage_TodoToday.Location = new System.Drawing.Point(4, 29);
+            this.tabPage_TodoToday.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tabPage_TodoToday.Name = "tabPage_TodoToday";
+            this.tabPage_TodoToday.Padding = new System.Windows.Forms.Padding(16);
+            this.tabPage_TodoToday.Size = new System.Drawing.Size(1250, 631);
+            this.tabPage_TodoToday.TabIndex = 0;
+            this.tabPage_TodoToday.Text = "Today";
+            this.tabPage_TodoToday.UseVisualStyleBackColor = true;
+            // 
+            // groupBox_Action
+            // 
+            this.groupBox_Action.Controls.Add(this.button_ActionModify);
+            this.groupBox_Action.Controls.Add(this.button_ActionDelete);
+            this.groupBox_Action.Controls.Add(this.button_ActionAdd);
+            this.groupBox_Action.Controls.Add(this.button_ActionSave);
+            this.groupBox_Action.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox_Action.Location = new System.Drawing.Point(16, 485);
+            this.groupBox_Action.Name = "groupBox_Action";
+            this.groupBox_Action.Padding = new System.Windows.Forms.Padding(16);
+            this.groupBox_Action.Size = new System.Drawing.Size(1218, 130);
+            this.groupBox_Action.TabIndex = 5;
+            this.groupBox_Action.TabStop = false;
+            this.groupBox_Action.Text = "Actions";
+            // 
+            // button_ActionModify
+            // 
+            this.button_ActionModify.Dock = System.Windows.Forms.DockStyle.Left;
+            this.button_ActionModify.Location = new System.Drawing.Point(216, 35);
+            this.button_ActionModify.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button_ActionModify.Name = "button_ActionModify";
+            this.button_ActionModify.Size = new System.Drawing.Size(100, 79);
+            this.button_ActionModify.TabIndex = 10;
+            this.button_ActionModify.Text = "Modify";
+            this.button_ActionModify.UseVisualStyleBackColor = true;
+            this.button_ActionModify.Click += new System.EventHandler(this.button_ActionModify_Click);
+            // 
+            // button_ActionDelete
+            // 
+            this.button_ActionDelete.Dock = System.Windows.Forms.DockStyle.Left;
+            this.button_ActionDelete.Location = new System.Drawing.Point(116, 35);
+            this.button_ActionDelete.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button_ActionDelete.Name = "button_ActionDelete";
+            this.button_ActionDelete.Size = new System.Drawing.Size(100, 79);
+            this.button_ActionDelete.TabIndex = 9;
+            this.button_ActionDelete.Text = "Delete";
+            this.button_ActionDelete.UseVisualStyleBackColor = true;
+            this.button_ActionDelete.Click += new System.EventHandler(this.button_ActionDelete_Click);
+            // 
+            // button_ActionAdd
+            // 
+            this.button_ActionAdd.Dock = System.Windows.Forms.DockStyle.Left;
+            this.button_ActionAdd.Location = new System.Drawing.Point(16, 35);
+            this.button_ActionAdd.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button_ActionAdd.Name = "button_ActionAdd";
+            this.button_ActionAdd.Size = new System.Drawing.Size(100, 79);
+            this.button_ActionAdd.TabIndex = 5;
+            this.button_ActionAdd.Text = "Add";
+            this.button_ActionAdd.UseVisualStyleBackColor = true;
+            this.button_ActionAdd.Click += new System.EventHandler(this.button_ActionAdd_Click);
+            // 
+            // button_ActionSave
+            // 
+            this.button_ActionSave.Dock = System.Windows.Forms.DockStyle.Right;
+            this.button_ActionSave.Location = new System.Drawing.Point(1102, 35);
+            this.button_ActionSave.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button_ActionSave.Name = "button_ActionSave";
+            this.button_ActionSave.Size = new System.Drawing.Size(100, 79);
+            this.button_ActionSave.TabIndex = 3;
+            this.button_ActionSave.Text = "Save";
+            this.button_ActionSave.UseVisualStyleBackColor = true;
+            this.button_ActionSave.Click += new System.EventHandler(this.button_ActionSave_Click);
+            // 
+            // groupBox_TodoToday
+            // 
+            this.groupBox_TodoToday.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.groupBox_TodoToday.Controls.Add(this.checkedListBox_TodoToday);
+            this.groupBox_TodoToday.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox_TodoToday.Location = new System.Drawing.Point(16, 16);
+            this.groupBox_TodoToday.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox_TodoToday.Name = "groupBox_TodoToday";
+            this.groupBox_TodoToday.Padding = new System.Windows.Forms.Padding(24);
+            this.groupBox_TodoToday.Size = new System.Drawing.Size(1218, 461);
+            this.groupBox_TodoToday.TabIndex = 0;
+            this.groupBox_TodoToday.TabStop = false;
+            this.groupBox_TodoToday.Text = "...";
+            // 
+            // checkedListBox_TodoToday
+            // 
+            this.checkedListBox_TodoToday.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.checkedListBox_TodoToday.FormattingEnabled = true;
+            this.checkedListBox_TodoToday.HorizontalScrollbar = true;
+            this.checkedListBox_TodoToday.IntegralHeight = false;
+            this.checkedListBox_TodoToday.Location = new System.Drawing.Point(24, 43);
+            this.checkedListBox_TodoToday.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.checkedListBox_TodoToday.MinimumSize = new System.Drawing.Size(4, 100);
+            this.checkedListBox_TodoToday.Name = "checkedListBox_TodoToday";
+            this.checkedListBox_TodoToday.Size = new System.Drawing.Size(1170, 394);
+            this.checkedListBox_TodoToday.TabIndex = 0;
+            this.checkedListBox_TodoToday.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.checkedListBox_TodoToday_ItemCheck);
+            // 
+            // tabPage_TodoOld
+            // 
+            this.tabPage_TodoOld.AutoScroll = true;
+            this.tabPage_TodoOld.Location = new System.Drawing.Point(4, 29);
+            this.tabPage_TodoOld.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tabPage_TodoOld.Name = "tabPage_TodoOld";
+            this.tabPage_TodoOld.Padding = new System.Windows.Forms.Padding(16);
+            this.tabPage_TodoOld.Size = new System.Drawing.Size(1250, 631);
+            this.tabPage_TodoOld.TabIndex = 1;
+            this.tabPage_TodoOld.Text = "Previously";
+            this.tabPage_TodoOld.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1258, 664);
+            this.Controls.Add(this.tabControl_Main);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MinimumSize = new System.Drawing.Size(640, 480);
+            this.Name = "Form1";
+            this.Text = "Daily Task Planner";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.tabControl_Main.ResumeLayout(false);
+            this.tabPage_TodoToday.ResumeLayout(false);
+            this.groupBox_Action.ResumeLayout(false);
+            this.groupBox_TodoToday.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TabControl tabControl_Main;
+        private System.Windows.Forms.TabPage tabPage_TodoToday;
+        private System.Windows.Forms.TabPage tabPage_TodoOld;
+        private System.Windows.Forms.GroupBox groupBox_TodoToday;
+        private System.Windows.Forms.Button button_ActionSave;
+        private System.Windows.Forms.CheckedListBox checkedListBox_TodoToday;
+        private System.Windows.Forms.GroupBox groupBox_Action;
+        private System.Windows.Forms.Button button_ActionAdd;
+        private System.Windows.Forms.Button button_ActionModify;
+        private System.Windows.Forms.Button button_ActionDelete;
+    }
+}
+
---
+++ DailyTaskPlanner/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace DailyTaskPlanner
+{
+    public partial class Form1 : Form
+    {
+        // Variables
+        bool hasUnsavedModifications = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Form1_Resize(null, EventArgs.Empty);
+
+            // Set today's date
+            groupBox_TodoToday.Text = DateTime.Today.ToString("dd.MM.yyyy");
+
+            // Load old tasks
+            string path = "tasks";
+            if (Directory.Exists(path))
+            {
+                string[] files = Directory.GetFiles(path, "*.csv");
+                // Order the files by date
+                files = files.OrderBy(f => DateTime.ParseExact(Path.GetFileNameWithoutExtension(f), "dd.MM.yyyy", CultureInfo.InvariantCulture)).ToArray();
+
+                foreach (string file in files)
+                {
+                    // Check if it's today's tasks
+                    bool isTodaysTask = Path.GetFileNameWithoutExtension(file) == DateTime.Today.ToString("dd.MM.yyyy");
+
+                    GroupBox groupBox_TodoOld = new GroupBox();
+                    CheckedListBox checkedListBox_TodoOld = new CheckedListBox();
+                    checkedListBox_TodoOld.Dock = DockStyle.Fill;
+                    checkedListBox_TodoOld.HorizontalScrollbar = true;
+                    checkedListBox_TodoOld.Enabled = false;
+
+                    using (StreamReader sr = new StreamReader(file))
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            string line = sr.ReadLine();
+                            string[] data = line.Split(';');
+                            checkedListBox_TodoOld.Items.Add(data[1], Convert.ToBoolean(data[0]));
+
+                            // Load today's tasks (if from current date)
+                            if (isTodaysTask) checkedListBox_TodoToday.Items.Add(data[1], Convert.ToBoolean(data[0]));
+                        }
+                    }
+
+                    groupBox_TodoOld.Padding = new Padding(16);
+                    groupBox_TodoOld.Dock = DockStyle.Top;
+                    groupBox_TodoOld.Text = Path.GetFileNameWithoutExtension(file);
+                    // Calculate the minimum size based on the number of tasks
+                    groupBox_TodoOld.MinimumSize = new Size(1, Math.Max((int)Math.Round(23.333f * 6), (int)Math.Round(23.333f * checkedListBox_TodoOld.Items.Count)));
+                    groupBox_TodoOld.Controls.Add(checkedListBox_TodoOld);
+
+                    // Add todo (if not from current date)
+                    if (!isTodaysTask) tabPage_TodoOld.Controls.Add(groupBox_TodoOld);
+                }
+            }
+        }
+
+        // Shows an input box for the user to enter a text string
+        private string ShowInputBox(string title= "Enter your text", string defaultText="")
+        {
+            using (var form = new Form())
+            {
+                form.FormBorderStyle = FormBorderStyle.FixedDialog;
+                form.StartPosition = FormStartPosition.CenterScreen;
+                form.MinimizeBox = false;
+                form.MaximizeBox = false;
+                form.Width = 480;
+                form.Text = title;
+
+                int margin = 5;
+
+                var textBox = new TextBox()
+                {
+                    Left = margin,
+                    Top = margin,
+                    Width = form.Width - (margin * 5),
+                    Text = defaultText,
+                };
+
+                var buttonOk = new Button()
+                {
+                    Text = "OK",
+                    Left = form.Width - (margin * 4) - 75,
+                    Width = 75,
+                    Top = textBox.Height + (margin * 2),
+                    DialogResult = DialogResult.OK,
+                };
+
+                form.Height = (textBox.Height + buttonOk.Height + margin) * 2;
+
+                form.Controls.Add(textBox);
+                form.Controls.Add(buttonOk);
+                form.AcceptButton = buttonOk;
+
+                var result = form.ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    // Returns the entered text
+                    return textBox.Text;
+                }
+            }
+
+            // Returns the default text if the dialog is cancelled
+            return defaultText;
+        }
+
+        // Returns the default text if the dialog is cancelled
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            groupBox_TodoToday.Height = ClientSize.Height - groupBox_Action.Height - 48;
+        }
+
+        // Returns the default text if the dialog is cancelled
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (hasUnsavedModifications)
+            {
+                DialogResult result = MessageBox.Show("Do you really want to quit without saving the modifications?", "Attention", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (result == DialogResult.No)
+                {
+                    e.Cancel = true;
+                }
+            }
+        }
+
+        // Adds a new item to the 'Todo Today' list
+        private void button_ActionAdd_Click(object sender, EventArgs e)
+        {
+            int index = checkedListBox_TodoToday.SelectedIndex;
+            if (index == -1)
+            {
+                checkedListBox_TodoToday.Items.Add(ShowInputBox("Enter the new text for the item to add", ""));
+                hasUnsavedModifications = true;
+            }
+            else
+                checkedListBox_TodoToday.Items.Insert(index + 1, ShowInputBox("Enter the new text for the item to add", ""));
+        }
+
+        // Deletes the selected item from the 'Todo Today' list
+        private void button_ActionDelete_Click(object sender, EventArgs e)
+        {
+            if (checkedListBox_TodoToday.SelectedIndex != -1)
+            {
+                checkedListBox_TodoToday.Items.RemoveAt(checkedListBox_TodoToday.SelectedIndex);
+                hasUnsavedModifications= true;
+            }
+            else
+                MessageBox.Show("Select an item before attempting to delete it.");
+        }
+
+        // Modify the selected item from the 'Todo Today' list
+        private void button_ActionModify_Click(object sender, EventArgs e)
+        {
+            if (checkedListBox_TodoToday.SelectedIndex != -1)
+            {
+                checkedListBox_TodoToday.Items[checkedListBox_TodoToday.SelectedIndex] = ShowInputBox("Enter the new text for the selected item.", checkedListBox_TodoToday.SelectedItem.ToString());
+                hasUnsavedModifications = true;
+            }
+            else
+                MessageBox.Show("You must select an item before making modifications to it.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        // Save the 'Todo Today' list
+        private void button_ActionSave_Click(object sender, EventArgs e)
+        {
+            string path = "tasks";
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+
+            using (StreamWriter sw = new StreamWriter(Path.Combine(path, groupBox_TodoToday.Text + ".csv")))
+            {
+                for (int i = 0; i < checkedListBox_TodoToday.Items.Count; i++)
+                    sw.WriteLine(checkedListBox_TodoToday.GetItemChecked(i) + ";" + checkedListBox_TodoToday.Items[i].ToString());
+            }
+
+            hasUnsavedModifications = false;
+        }
+
+        // Check event triggered for an item in the 'Todo Today' list.
+        private void checkedListBox_TodoToday_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            hasUnsavedModifications = true;
+        }
+    }
+}
